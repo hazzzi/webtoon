571a6dc184ee47de863473e14aba3ebd78b1a2d1
Hanhye <39878484+Hanhye@users.noreply.github.com>
2018-12-06 14:04:44 +0900

    board

--- WebToon/src/net/board/controller/FanBoardFrontController.java
+++ WebToon/src/net/board/controller/FanBoardFrontController.java
@@ -2,43 +2,79 @@ package net.board.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.board.action.FanBoardList;
+import net.board.action.fanboardWriteAction;
 
 public class FanBoardFrontController extends HttpServlet {
-	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		String requestURI = request.getRequestURI();
 		String contextPath = request.getContextPath();
 		String command = requestURI.substring(contextPath.length());
 
-		System.out.println("가상 주소 확인: " + command);	
-		
+		System.out.println("가상 주소 확인: " + command);
+
 		ActionForward forward = null;
 		Action action = null;
-		
-		
-		
-		if(command.equals("fanboardWriteAction.bo")){
-			
-			
-			
-		}else if(command.equals("/fanboardList.bo")){
-			
-			action = new FanBoardList();
-			
-			try{
+
+		if (command.equals("/fanboardWrite.fo")) {
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./board/fan_writingPage.jsp");
+
+		}else if (command.equals("/fanboardWriteAction.fo")){
+			action = new fanboardWriteAction();
+
+			try {
 				forward = action.execute(request, response);
-			}catch (Exception e) {
+			} catch (Exception e) {
+				e.printStackTrace();
+			}			
+
+			
+		} else if (command.equals("/fanboardList.fo")) {
+
+			action = new FanBoardList();
+
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
-		}else if(command.equals("")){
-			
+
+		} 
+
+		// 이동 시작
+		if (forward != null) {
+			if (forward.isRedirect()) {
+				// true sendRedirect()
+				response.sendRedirect(forward.getPath());
+			} else {
+				// false forward()
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
 		}
+		// 이동 끝
 	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
 }
