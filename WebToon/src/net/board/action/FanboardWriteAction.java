f5f9ee0c3f19a41e390faa27e78a956358277d84
ITWILL <ITWILL@DESKTOP-L21M6GU>
2018-12-10 20:40:27 +0900

    ..

--- WebToon/src/net/board/action/FanboardWriteAction.java
+++ WebToon/src/net/board/action/FanboardWriteAction.java
@@ -1,5 +1,7 @@
 package net.board.action;
 
+import java.io.PrintWriter;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,22 +34,34 @@ public class FanboardWriteAction implements Action {
 	    FanBean fb = new FanBean();
 		HttpSession session = request.getSession();
 		String mem_num = (String)session.getAttribute("mem_num");	    
-	    
+		ActionForward forward = new ActionForward();
+		
+		String fa_subject = multi.getParameter("fa_subject");
+		String fa_category1 = multi.getParameter("fa_category1");
+		String fa_category2 = multi.getParameter("fa_category2");
+		String fa_content = multi.getParameter("fa_content");
+		String fa_img = multi.getFilesystemName("fa_img");
+		
+		
 		if (mem_num == null) {
-			ActionForward forward = new ActionForward();
+			
 			forward.setRedirect(true);
 			forward.setPath("./login.me");
 			
 			return forward;
 			
-		}else {
-			
-			String fa_subject = multi.getParameter("fa_subject");
-			String fa_category1 = multi.getParameter("fa_category1");
-			String fa_category2 = multi.getParameter("fa_category2");
-			String fa_content = multi.getParameter("fa_content");
-			String fa_img = multi.getFilesystemName("fa_img");
-			
+		}else if(fa_img==null){
+				response.setContentType("text/html; charset=UTF-8");
+				PrintWriter out=response.getWriter();
+				out.println("<script>");
+				out.println("alert('사진 첨부는 필수 사항입니다.');");
+				out.println("history.back();");
+				out.println("</script>");
+				out.close();
+			}else{
+			forward.setPath("./fanboardList.bo");
+			forward.setRedirect(true);	
+				
 			fb.setFa_mem_num(mem_num);
 			fb.setFa_category1(fa_category1);
 			fb.setFa_category2(fa_category2);
@@ -60,10 +74,7 @@ public class FanboardWriteAction implements Action {
 			System.out.println("이미지"+fa_img);
 			
 			fdao.insertFan(fb);
-			
-			ActionForward forward = new ActionForward();
-			forward.setPath("./fanboardList.bo");
-			forward.setRedirect(true);
+			}
 			
 			return forward;
 
@@ -71,4 +82,4 @@ public class FanboardWriteAction implements Action {
 		
 	}
 	
-}
+
