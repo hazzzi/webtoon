da284cc949edc22bcc08bfb5a01cf58a7a4bc647
LSH <whitesantia@naver.com>
2018-12-12 20:41:21 +0900

    회원 가입 유효성 검사

--- WebToon/src/net/member/action/MemberUpdateAction2.java
+++ WebToon/src/net/member/action/MemberUpdateAction2.java
@@ -1,5 +1,7 @@
 package net.member.action;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -12,7 +14,7 @@ import net.member.controller.ActionForward;
 import net.member.db.MemberBean;
 import net.member.db.MemberDAO;
 
-public class MemberUpdateAction2 implements Action{
+public class MemberUpdateAction2 implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -20,13 +22,13 @@ public class MemberUpdateAction2 implements Action{
 		System.out.println("MemberUpdateAction2 execute ");
 		request.setCharacterEncoding("utf-8");
 		String filePath = request.getRealPath("/upload");
-		System.out.println("파일 패스: "+filePath);
-		int maxSize=5*1024*1024;//5M
+		System.out.println("파일 패스: " + filePath);
+		int maxSize = 5 * 1024 * 1024;// 5M
 		// MultipartRequest multi 객체생성
-		MultipartRequest multi=
-				new MultipartRequest(request, filePath, maxSize,"utf-8",new DefaultFileRenamePolicy());
-		HttpSession session =request.getSession();
-		String mem_num=(String)session.getAttribute("mem_num");
+		MultipartRequest multi = new MultipartRequest(request, filePath, maxSize, "utf-8",
+				new DefaultFileRenamePolicy());
+		HttpSession session = request.getSession();
+		String mem_num = (String) session.getAttribute("mem_num");
 		MemberBean mb = new MemberBean();
 		mb.setNum(mem_num);
 		mb.setProfileimg(multi.getFilesystemName("file"));
@@ -36,17 +38,56 @@ public class MemberUpdateAction2 implements Action{
 		mb.setHint(multi.getParameter("sel_hint"));
 		mb.setHintans(multi.getParameter("hint_ans"));
 		mb.setAges(multi.getParameter("sel_age"));
-		System.out.println("파일 스트링 확인 "+filePath+multi.getFilesystemName("file"));
+		System.out.println("파일 스트링 확인 " + filePath + multi.getFilesystemName("file"));
 		MemberDAO mdao = new MemberDAO();
-		mdao.updateMember(mb);
-		
-		session.setAttribute("mem_nik", mb.getNik());
-		
+
+		// mb2는 업데이트시에 기존 정보와 다를 경우에 닉네임과 이메일이 중복되면 안되기 때문에 그걸 체크하기 위해 만들었습니다
+		MemberBean mb2 = mdao.getMember(mem_num);
+
 		ActionForward forward = new ActionForward();
-		forward.setRedirect(true);
-		forward.setPath("./myProfile.me");
+
 		
+
+			/*if (mdao.emailOverlapcheck(mb)) {
+				forward.setRedirect(true);
+				forward.setPath("./login.me");
+
+				response.setContentType("text/html; charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				out.println("<script>");
+				out.println("alert('이미 있는 이메일입니다..');");
+				out.println("history.back();");
+				out.println("</script>");
+				out.close();
+			} else if (mdao.nikOverlapcheck(mb)) {
+				forward.setRedirect(true);
+				forward.setPath("./login.me");
+
+				response.setContentType("text/html; charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				out.println("<script>");
+				out.println("alert('이미 있는 닉네임입니다..');");
+				out.println("history.back();");
+				out.println("</script>");
+				out.close();
+			} else {
+
+				mdao.updateMember(mb);
+
+				session.setAttribute("mem_nik", mb.getNik());
+
+				forward.setRedirect(true);
+				forward.setPath("./myProfile.me");
+			}*/
+			
+			mdao.updateMember(mb);
+
+			session.setAttribute("mem_nik", mb.getNik());
+
+			forward.setRedirect(true);
+			forward.setPath("./myProfile.me");
+	
 		return forward;
 	}
-	
+
 }
